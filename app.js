c99d50a90f0098aacf6313d0022a8f212ab7bb37
amath vaillam <amathv01@gmail.com>
2020-07-12 22:58:12 +0000

    4.1 Blog list, step1

---
+++ app.js
@@ -0,0 +1,42 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+var morgan = require('morgan')
+const blogRouter = require('./controllers/blog')
+
+logger.info('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connection to MongoDB:', error.message)
+    })
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use(morgan(function (tokens, req, res) {
+    return [
+        tokens.method(req, res),
+        tokens.url(req, res),
+        tokens.status(req, res),
+        tokens.res(req, res, 'content-length'), '-',
+        tokens['response-time'](req, res), 'ms',
+        `{${Object.entries(req.body)
+            .map(item => `"${item[0]}":"${item[1]}"`)
+            .join(',')
+        }}`].join(' ')
+}))
+
+app.use('/api/blogs', blogRouter)
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
