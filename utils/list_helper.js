e2ea8423685b0eaf3532270df0be1723f9809e3f
amath vaillam <amathv01@gmail.com>
2020-07-13 01:50:54 +0000

    4.5*: helper functions and unit tests, step3

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -11,7 +11,27 @@ const totalLikes = (blogs) => {
     }
 }
 
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0)
+        return {}
+    else if (blogs.length === 1){
+        return {
+            title: blogs[0].title,
+            author: blogs[0].author,
+            likes: blogs[0].likes
+        }
+    }
+    else {
+        const blog = blogs.reduce((acc, curr) => (!acc.likes || acc.likes < curr.likes) ? curr : acc , {})
+        return {
+            title: blog.title,
+            author: blog.author,
+            likes: blog.likes
+        }
+    }
+}
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
