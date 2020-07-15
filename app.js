380721947608ea1ece08d01a310210e1c6de7624
amath vaillam <amathv01@gmail.com>
2020-07-15 15:42:55 +0000

    4.8: Blog list tests, step1

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const config = require('./utils/config')
 const express = require('express')
 const app = express()
+
+require('express-async-errors')
 const cors = require('cors')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
@@ -21,8 +23,8 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
-app.use(middleware.requestLogger)
-app.use(morgan(function (tokens, req, res) {
+//app.use(middleware.requestLogger)
+/*app.use(morgan(function (tokens, req, res) {
     return [
         tokens.method(req, res),
         tokens.url(req, res),
@@ -33,7 +35,7 @@ app.use(morgan(function (tokens, req, res) {
             .map(item => `"${item[0]}":"${item[1]}"`)
             .join(',')
         }}`].join(' ')
-}))
+}))*/
 
 app.use('/api/blogs', blogRouter)
 app.use(middleware.unknownEndpoint)
---
+++ tests/blog_api.test.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const Blog = require('../models/blog')
+const app = require('../app')
+const helper = require('../utils/list_helper')
+
+const api = supertest(app)
+
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    
+    for (let blog of helper.initialBlogs) {
+        let blogObject = new Blog(blog)
+        await blogObject.save()
+      }
+})
+test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(helper.initialBlogs.length)
+  })
+  
+  afterAll(() => {
+    mongoose.connection.close()
+  })
