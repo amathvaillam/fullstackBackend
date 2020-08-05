e445da754572fb4ba40069a8d3d14212b3137040
amath vaillam <amathv01@gmail.com>
2020-08-05 15:40:24 +0000

    4.19: bloglist expansion, step7

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,11 +1,21 @@
 const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
+
 
 blogRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user')
     response.json(blogs)
 })
+
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+      return authorization.substring(7)
+    }
+    return null
+}
 
 
 blogRouter.post('/', async (request, response) => {
@@ -14,7 +24,13 @@ blogRouter.post('/', async (request, response) => {
         return response.status(400).json({ error: "Bad Request" })
     }
 
-    const user = await User.findById(userId)
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if (!token || !decodedToken.id) {
+        return response.status(401).json({ error: 'token missing or invalid' })
+    }
+  
+      const user = await User.findById(decodedToken.id)
     const blog = new Blog({
         likes: 0,
         url,
--- controllers/user.js
+++ controllers/user.js
@@ -3,6 +3,14 @@ const bcrypt = require('bcryptjs')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
+      return authorization.substring(7)
+    }
+    return null
+}
+
 usersRouter.post('/', async (request, response) => {
     const body = request.body
     const passwordCheck = pass => pass.length < 3
